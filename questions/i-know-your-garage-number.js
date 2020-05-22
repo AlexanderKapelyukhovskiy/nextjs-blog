afbff9fcb9c98650ee12fbb509141fc4a0f2d1ea
Alex Kapelyukhovsky <alkapa2005@gmail.com>
2020-05-22 22:32:13 +0300

    one more test

---
+++ questions/i-know-your-garage-number.js
@@ -0,0 +1,41 @@
+const data = {
+  title: "как хорошо ты знаешь номера гаражей?",
+  date: "2020-05-22",
+  questions: [
+    {
+      questionId: 1,
+      question: "какой номер гараж у Коли?",
+      answers: [
+        { answerId: 1, answer: "666" },
+        { answerId: 2, answer: "141" },
+        { answerId: 3, answer: "140" },
+        { answerId: 4, answer: "139" },
+      ],
+      correctAnswerId: 2,
+    },
+    {
+      questionId: 2,
+      question: "какой номер гараж у Саши Н.?",
+      answers: [
+        { answerId: 1, answer: "777" },
+        { answerId: 2, answer: "144" },
+        { answerId: 3, answer: "140" },
+        { answerId: 4, answer: "143" },
+      ],
+      correctAnswerId: 4,
+    },
+    {
+      questionId: 3,
+      question: "какой номер гараж у Саши К.?",
+      answers: [
+        { answerId: 1, answer: "144" },
+        { answerId: 2, answer: "149" },
+        { answerId: 3, answer: "141" },
+        { answerId: 4, answer: "135" },
+      ],
+      correctAnswerId: 1,
+    },
+  ],
+};
+
+export default data;
