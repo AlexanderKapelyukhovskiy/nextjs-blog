dea66e78c51ec7d6d538154a71727eb8e3c0f4ba
Alex Kapelyukhovsky <alkapa2005@gmail.com>
2020-05-22 22:06:32 +0300

    New test

---
+++ questions/i-know-your-birthday-2.js
@@ -0,0 +1,41 @@
+const data = {
+  title: "как хорошо ты знаешь дни рождения у наших Киевлян?",
+  date: "2020-05-22",
+  questions: [
+    {
+      questionId: 1,
+      question: "когда день рождения у Маши?",
+      answers: [
+        { answerId: 1, answer: "30.05" },
+        { answerId: 2, answer: "30.04" },
+        { answerId: 3, answer: "29.04" },
+        { answerId: 4, answer: "11.01" },
+      ],
+      correctAnswerId: 2,
+    },
+    {
+      questionId: 2,
+      question: "когда день рождения у Лены?",
+      answers: [
+        { answerId: 1, answer: "05.09" },
+        { answerId: 2, answer: "05.08" },
+        { answerId: 3, answer: "04.02" },
+        { answerId: 4, answer: "07.07" },
+      ],
+      correctAnswerId: 1,
+    },
+    {
+      questionId: 3,
+      question: "когда день рождения у Саши?",
+      answers: [
+        { answerId: 1, answer: "24.08" },
+        { answerId: 2, answer: "23.11" },
+        { answerId: 3, answer: "22.07" },
+        { answerId: 4, answer: "24.11" },
+      ],
+      correctAnswerId: 4,
+    },
+  ],
+};
+
+export default data;
