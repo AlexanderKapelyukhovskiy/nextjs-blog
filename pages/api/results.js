649fd2c8480412cb9e86b63d8a09a27d59038852
Alex Kapelyukhovsky <alkapa2005@gmail.com>
2020-05-22 21:10:31 +0300

    investigate mongodb error

--- pages/api/results.js
+++ pages/api/results.js
@@ -3,10 +3,18 @@ import { insertAnswer, getAnswers } from "../../lib/questionDb";
 export default async (req, res) => {
   if (req.method === "POST") {
     console.log(req.body);
-    let result = await insertAnswer(req.body);
-    res.status(201).json(result);
+    try {
+      let result = await insertAnswer(req.body);
+      res.status(201).json(result);
+    } catch (error) {
+      res.status(400).send(error);
+    }
   } else {
-    var answers = await getAnswers();
-    res.status(200).json([...answers]);
+    try {
+      var answers = await getAnswers();
+      res.status(200).json([...answers]);
+    } catch (error) {
+      res.status(400).send(JSON.stringify(error) + error.stack);
+    }
   }
 };
