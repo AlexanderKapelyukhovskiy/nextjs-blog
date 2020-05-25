35e4a29365187a044552e2d8cfd3dfb08995cf73
Alex Kapelyukhovsky <alkapa2005@gmail.com>
2020-05-25 23:24:59 +0300

    debug api 2

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,8 +1,8 @@
-import { getQuestionData } from "../../lib/questions";
+import { getQuestionDataTest } from "../../lib/questions";
 
 export default (req, res) => {
   try {
-    res.status(200).json(getQuestionData("i-know-your-birthday1"));
+    res.status(200).json(getQuestionDataTest("i-know-your-birthday"));
   } catch (error) {
     res.status(400).json(error.stack);
   }
