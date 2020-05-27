325dc22adc65f3f0a7ab0f9e1f64ce59edd281be
Alex Kapelyukhovsky <alkapa2005@gmail.com>
2020-05-27 22:23:13 +0300

    add test

---
+++ questions/how-good-you-in-math.js
@@ -0,0 +1,41 @@
+const data = {
+  title: "how good are you in math?",
+  date: "2020-05-27",
+  questions: [
+    {
+      questionId: 1,
+      question: "100+20*3?",
+      answers: [
+        { answerId: 1, answer: "132" },
+        { answerId: 2, answer: "100" },
+        { answerId: 3, answer: "160" },
+        { answerId: 4, answer: "321" },
+      ],
+      correctAnswerId: 3,
+    },
+    {
+      questionId: 2,
+      question: "99/1-40+20*2?",
+      answers: [
+        { answerId: 1, answer: "74" },
+        { answerId: 2, answer: "99" },
+        { answerId: 3, answer: "100" },
+        { answerId: 4, answer: "55" },
+      ],
+      correctAnswerId: 2,
+    },
+    {
+      questionId: 3,
+      question: "(59-7*8+5)*9",
+      answers: [
+        { answerId: 1, answer: "81" },
+        { answerId: 2, answer: "73" },
+        { answerId: 3, answer: "64" },
+        { answerId: 4, answer: "72" },
+      ],
+      correctAnswerId: 4,
+    },
+  ],
+};
+
+export default data;
